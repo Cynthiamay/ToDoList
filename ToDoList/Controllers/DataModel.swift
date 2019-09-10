9bd8ff570f17c55dee61336fa3a4c370fbdd2bad
Cynthia <cynthiayamaoto@gmail.com>
2019-09-10 09:50:56 -0300

    Adição da barra de pesquisa e banco de dados SQL.

---
+++ ToDoList/Controllers/DataModel.swift
@@ -0,0 +1,9 @@
+//
+//  DataModel.swift
+//  ToDoList
+//
+//  Created by Treinamento on 9/6/19.
+//  Copyright © 2019 cynthiamayumiwatanabeyamaoto. All rights reserved.
+//
+
+import Foundation
--- ToDoList/Controllers/TodoListViewController.swift
+++ ToDoList/Controllers/TodoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
@@ -16,12 +17,18 @@ class TodoListViewController: UITableViewController {
     //Definir o que será feito, o que estará na lista
     var itemArray = [Item]()
     //outra forma de manter os dados sem ser UserDefault
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(dataFilePath)
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+
+        
+        
+        //print(dataFilePath)
         
         
 //        let newItem = Item()
@@ -36,6 +43,7 @@ class TodoListViewController: UITableViewController {
 //        newItem3.title = "Fazer isto"
 //        itemArray.append(newItem3)
         
+
         loadItems()
         
         // Do any additional setup after loading the view.
@@ -83,8 +91,15 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     // A função responsável por fazer sumir quando se clica em qualquer célula na table view
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //delete
         
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
+        
+        //update the datas
+        //itemArray[indexPath.row].setValue("Completed", forKey: "title")
+        
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
         
@@ -136,24 +151,17 @@ class TodoListViewController: UITableViewController {
             
             //o que acontece quando o usuário clica no botão de mais no alerta
             //print("Success!")
-            let newItem = Item()
+            
+            let newItem = Item(context: self.context)
 
             newItem.title = textField.text!
+            newItem.done = false
             
             self.itemArray.append(newItem)
         
             //self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            let encoder = PropertyListEncoder()
         
-            do{
-                let data = try encoder.encode(self.itemArray)
-                try data.write(to: self.dataFilePath!)
-            }catch{
-                print("Error encoding item array, \(error)")
-                
-                
-            }
-            self.tableView.reloadData()
+    
             
             self.saveItems()
             }
@@ -175,28 +183,61 @@ class TodoListViewController: UITableViewController {
     //MARK - Model Manipulation Methods
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
         
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+         try context.save()
         } catch{
-            print("Error encoding item array, \(error)")
+            print("Error saving Context \(error)")
             
             
         }
         self.tableView.reloadData()
     }
-    func loadItems() {
-        if let data = try? Data (contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch {
-                print("Error decoding item Array, \(error)")
-            }
+    
+    //READ - Load up items from our system container, é preciso especificar o tipo do dado
+    func loadItems(with request:NSFetchRequest<Item> = Item.fetchRequest()) {
+
+        //let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do{
+        itemArray = try context.fetch(request)
+        }
+        catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
     }
-      
 }
+// MARK = Search bar methods
+extension TodoListViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        //o que queremos de volta da pesquisa
+        
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        
+        loadItems(with: request)
+       
+        
+    }
+    //Depois que sai da pesquisa, a lista volta ao normal
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)  {
+        if searchBar.text?.count == 0{
+            loadItems()
+
+            //não congela o usuário enquanto estiver procurando por dados da internet
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+
+        }
+    }
+    
+    
 }
